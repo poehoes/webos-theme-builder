9bfb3b158acfe9eb03427d998974cff7cac294d9
robitaille88 <robitaille88@7cb5f81e-e046-11de-81d3-db62269da9c1>
2009-12-03 20:24:27 +0000

    Source

---
+++ WebOSThemeBuilder/src/ca/canucksoftware/themebuilder/FileEntry.java
@@ -0,0 +1,17 @@
+package ca.canucksoftware.themebuilder;
+
+import java.io.File;
+
+/**
+ *
+ * @author Jason
+ */
+public class FileEntry {
+    public String dest;
+    public File file;
+    
+    public FileEntry() {
+        dest = null;
+        file = null;
+    }
+}
